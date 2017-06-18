2439c3f6db23de3d5c968db1e807e966b9b14a80
binhle <levanbinh1605@gmail.com>
2017-06-18 11:35:42 +0700

    remove

--- src/main/java/com/binhle/vspelling/Common/ErrorConstants.java
+++
@@ -1,8 +0,0 @@
-package com.binhle.vspelling.Common;
-
-/**
- * Created by BinhLe on 4/16/2017.
- */
-public class ErrorConstants {
-    public static final String RESOURCE_NAME_NOT_FOUND = "The Name Of Resource ID %s was not found";
-}
